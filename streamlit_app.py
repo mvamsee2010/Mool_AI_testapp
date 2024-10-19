6d8e4e11866a4f8ebf9fd3a28ce8884c2a9b1fad
mvamsee2010 <135558592+mvamsee2010@users.noreply.github.com>
2024-10-19 16:57:56 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,7 +6,7 @@ st.title('🦜🔗 Quickstart App')
 openai_api_key = st.sidebar.text_input('OpenAI API Key', type='password')
 
 def generate_response(input_text):
-    llm = OpenAI(temperature=0.7, openai_api_key=openai_api_key)
+    llm = OpenAI(temperature=0.7, model_name="gpt-3.5-turbo", openai_api_key=openai_api_key)
     st.info(llm(input_text))
 
 with st.form('my_form'):
